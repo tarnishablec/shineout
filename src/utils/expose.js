c5419d3915eafa6195869854d85bc8949985e32e
EastblueOkay <eastblue_lan@126.com>
2019-05-17 14:02:15 +0800

    move to bugfix: select onCreate

--- src/utils/expose.js
+++ src/utils/expose.js
@@ -1,8 +1,11 @@
 import { isObject } from './is'
 import { exposeClass } from '../styles/expose'
+import { getUidStr } from './uid'
+import config from '../config'
 
 const types = ['primary', 'warning', 'danger', 'success', 'secondary']
 const attrs = ['background', 'color', 'border']
+const styleReplaceUid = `shineout_STYLE_HR_${getUidStr()}`
 
 function validateFormat(data) {
   if (!isObject(data)) {
@@ -41,6 +44,19 @@ function getColor(type) {
   return color
 }
 
+function styleReplace(text, id) {
+  let style = document.head.querySelector(`#${id}`)
+  if (style) {
+    style.innerHTML = text
+    return
+  }
+  style = document.createElement('style')
+  style.type = 'text/css'
+  style.id = id
+  style.innerHTML = text
+  document.head.appendChild(style)
+}
+
 export const color = {
   get primary() {
     return getColor('primary')
@@ -57,7 +73,16 @@ export const color = {
   get secondary() {
     return getColor('secondary')
   },
+  set set(options) {
+    if (!options) return
+    let text = ''
+    for (const [key, value] of Object.entries(options)) {
+      text += `.${config.prefix}-vars-${key} {${key.substring(key.indexOf('-') + 1)}: ${value}}`
+    }
+    styleReplace(text, styleReplaceUid)
+  },
 }
+window.color = color
 
 export const style = {
   getClassname,
